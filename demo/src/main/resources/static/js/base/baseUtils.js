bbd54fbc4445b56ed293e5fbcd49d92ac75de19e
sunshine2014zq <1475785135@qq.com>
2019-02-15 17:40:23 +0800

    JS工具-提示

--- demo/src/main/resources/static/js/base/baseUtils.js
+++ demo/src/main/resources/static/js/base/baseUtils.js
@@ -3,12 +3,20 @@
  */
 
 var baseUtils = {
+    //判断表单元素的值是否为空
     isEmpty : function (item) {
         if (item.val() == '' || item.val() == undefined) {
             return true;
         }
         return false;
     },
+    //1:成功 2:错误 3：疑问 4：锁定 5：哭脸，6：笑脸 7：警告
+    tip: function (msg, icon, time) {
+        layer.msg(msg, {icon: icon, time: time});
+    }
+
+
+
 
 
 
